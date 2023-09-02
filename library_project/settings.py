1c13e4978c944dd6fdcc301e1f19c8175dc339c0
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-09-02 04:17:37 +0100

    first commit

--- library_project/settings.py
+++ library_project/settings.py
@@ -140,7 +140,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 CORS_ORIGIN_WHITELIST = [
    'http://localhost:3000'
