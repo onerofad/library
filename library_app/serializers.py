f09246516de257a3e7456cd9463345d26c2764dd
frank@gmail.com <imafidonfrank2015@gmail.com>
2023-09-04 11:49:43 +0100

    first commit

--- library_app/serializers.py
+++ library_app/serializers.py
@@ -4,7 +4,7 @@ from .models import Author, Address, Student, Book, BookIssued, BookReturned
 class AuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Author
-        fields = ('name', 'about', 'author_image')
+        fields = ('name', 'about')
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
@@ -19,7 +19,7 @@ class StudentSerializer(serializers.ModelSerializer):
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('id','name', 'pub_date', 'no_pages', 'book_lang', 'book_dim', 'book_isbn10', 'book_isbn13', 'book_image', 'author', 'book_issued')
+        fields = ('id','name', 'pub_date', 'no_pages', 'book_lang', 'book_dim', 'book_isbn10', 'book_isbn13', 'author', 'book_issued')
 
 class BookIssuedSerializer(serializers.ModelSerializer):
     class Meta:
